3caab4e16933a6359a935b4180a7cc4e77018715
reda <ridaazyaiz@gmail.com>
2021-08-20 13:47:34 +0100

    create page works

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -73,5 +73,39 @@
                     </div>
                   </div>
                 </li>
+                {{-- page 2 works --}}
+                <li class="l-section section">
+                    <div class="work">
+                      <h2>Selected work</h2>
+                      <div class="work--lockup">
+                        <ul class="slider">
+                          <li class="slider--item slider--item-left">
+                            <a href="#0">
+                              <div class="slider--item-image">
+                                <img src="assets/img/work-victory.jpg" alt="Victory">
+                              </div>
+                              <p class="slider--item-title">Victory</p>
+                              <p class="slider--item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do.</p>
+                            </a>
+                          </li>
+                          <li class="slider--item slider--item-center">
+                            <a href="#0">
+                              <div class="slider--item-image">
+                                <img src="assets/img/work-metiew-smith.jpg" alt="Metiew and Smith">
+                              </div>
+                              <p class="slider--item-title">Metiew &amp; Smith</p>
+                              <p class="slider--item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do.</p>
+                            </a>
+                          </li>
+                          <li class="slider--item slider--item-right">
+                            <a href="#0">
+                              <div class="slider--item-image">
+                                <img src="assets/img/work-alex-nowak.jpg" alt="Alex Nowak">
+                              </div>
+                              <p class="slider--item-title">Alex Nowak</p>
+                              <p class="slider--item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do.</p>
+                            </a>
+                          </li>
+                        </ul>
 </body>
 </html>
