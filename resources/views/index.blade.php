a5fafe3e643b22098fb2a36aa2755dcc1ad98c45
reda <ridaazyaiz@gmail.com>
2021-08-20 13:25:33 +0100

    add css file and create index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <title>Abouhanifa abdellah</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="description" content="Portoflio">
+      <meta name="keywords" content="global, template, html, sass, jquery">
+      <meta name="author" content="Abouhanifa abdellah">
+</head>
+<body>
+    <div class="device-notification">
+        <a class="device-notification--logo" href="#0">
+          <img src="assets/img/logo.png" alt="Global">
+          <p>Global</p>
+        </a>
+        <p class="device-notification--message">Global has so much to offer that we must request you orient your device to portrait or find a larger screen. You won't be disappointed.</p>
+      </div>
+
+      <div class="perspective effect-rotate-left">
+        <div class="container"><div class="outer-nav--return"></div>
+          <div id="viewport" class="l-viewport">
+            <div class="l-wrapper">
+              <header class="header">
+                <a class="header--logo" href="#0">
+                  <img src="assets/img/logo.png" alt="Global">
+                  <p>Abouhanifa abdellah</p>
+                </a>
+                <button class="header--cta cta">Hire Us</button>
+                <div class="header--nav-toggle">
+                  <span></span>
+                </div>
+              </header>
+              <nav class="l-side-nav">
+                <ul class="side-nav">
+                  <li class="is-active"><span>Home</span></li>
+                  <li><span>Works</span></li>
+                  <li><span>About</span></li>
+                  <li><span>Contact</span></li>
+                  <li><span>Hire us</span></li>
+                </ul>
+              </nav>
+</body>
+</html>
